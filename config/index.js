f18cd7df5a320189778b0372bddb049faee65bd1
scriptchao <1154806236@qq.com>
2017-12-27 11:14:00 +0800

    'add'

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@
 const devHost = '127.0.0.1';
 const prodHost = 'www.scriptchao.xyz';
 const devPort = 8282;
-const prodPort = 80;
+const prodPort = 8080;
 
 export default {
     host: process.env.NODE_ENV === 'development' ? devHost : prodHost,
