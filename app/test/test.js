b587db046c8d9413c5037c7ddbde57b16e4759fb
scriptchao <1154806236@qq.com>
2017-12-29 15:11:06 +0800

    'add'

--- app/test/test.js
+++ app/test/test.js
@@ -89,116 +89,116 @@ class Test extends React.Component {
     render() {
         const {address} = this.mapStore;
         return (
-            localStorage.getItem(config.token) ?
-                <div className="test">
-                    {
-                        this.userInfo.nickname ?
-                            <div className="info">
-                                <div className="pic">
-                                    <img src={this.userInfo.imgUrl}/>
-                                </div>
-                                <div>
-                                    <span>{`姓名 ：${this.userInfo.nickname}`}</span><br/>
-                                    <span>{`性别 ：${this.userInfo.sex == 1 ? '男' : '女'}`}</span><br/>
-                                    <span>{`所在地 ：${this.userInfo.province} ${this.userInfo.city}`}</span>
-                                </div>
-                            </div> : null
-                    }
-                    <Button plain className="btn" onClick={() => {
-                        weChat.getNetworkType().then(networkType => {
-                            this.networkType = networkType;
-                            this.tip1 = true;
-                            setTimeout(() => {
-                                this.tip1 = false
 
-                            }, 2000)
-                        })
-                    }}>获取网络状态111
-                    </Button>
-                    <Button plain className="btn" onClick={() => {
-                        weChat.getLocation().then(data => {
-                            this.longitude = data.longitude;
-                            this.latitude = data.latitude;
-                            this.tip2 = true;
-                            setTimeout(() => {
-                                this.tip2 = false
+            <div className="test">
+                {
+                    this.userInfo.nickname ?
+                        <div className="info">
+                            <div className="pic">
+                                <img src={this.userInfo.imgUrl}/>
+                            </div>
+                            <div>
+                                <span>{`姓名 ：${this.userInfo.nickname}`}</span><br/>
+                                <span>{`性别 ：${this.userInfo.sex == 1 ? '男' : '女'}`}</span><br/>
+                                <span>{`所在地 ：${this.userInfo.province} ${this.userInfo.city}`}</span>
+                            </div>
+                        </div> : null
+                }
+                <Button plain className="btn" onClick={() => {
+                    weChat.getNetworkType().then(networkType => {
+                        this.networkType = networkType;
+                        this.tip1 = true;
+                        setTimeout(() => {
+                            this.tip1 = false
 
-                            }, 2000)
-                        })
-                    }}>获取经纬度
-                    </Button>
-                    <Button plain className="btn" onClick={this.getAddress.bind(this)}>获取具体位置</Button>
-                    <Button plain className="btn" onClick={() => {
-                        weChat.scanQRCode()
-                    }}>微信扫一扫
-                    </Button>
-                    <Button plain className="btn" onClick={() => {
-                        weChat.openAddress().then(data => {
-                        })
-                    }}>共享收货地址
-                    </Button>
-                    <Button plain className="btn" onClick={() => {
-                        weChat.chooseImage().then(localIds => {
-                            this.localIds = localIds;
-                            this.tip4 = true
-                        })
-                    }}>拍照或从手机相册中选图
-                    </Button>
+                        }, 2000)
+                    })
+                }}>获取网络状态111
+                </Button>
+                <Button plain className="btn" onClick={() => {
+                    weChat.getLocation().then(data => {
+                        this.longitude = data.longitude;
+                        this.latitude = data.latitude;
+                        this.tip2 = true;
+                        setTimeout(() => {
+                            this.tip2 = false
 
-                    <Button plain className="btn" onClick={() => {
-                        this.localIds.map(localId => {
-                            weChat.uploadImage(localId).then(serverId => {
-                                this.serverIds.push(serverId);
+                        }, 2000)
+                    })
+                }}>获取经纬度
+                </Button>
+                <Button plain className="btn" onClick={this.getAddress.bind(this)}>获取具体位置</Button>
+                <Button plain className="btn" onClick={() => {
+                    weChat.scanQRCode()
+                }}>微信扫一扫
+                </Button>
+                <Button plain className="btn" onClick={() => {
+                    weChat.openAddress().then(data => {
+                    })
+                }}>共享收货地址
+                </Button>
+                <Button plain className="btn" onClick={() => {
+                    weChat.chooseImage().then(localIds => {
+                        this.localIds = localIds;
+                        this.tip4 = true
+                    })
+                }}>拍照或从手机相册中选图
+                </Button>
 
-                            })
+                <Button plain className="btn" onClick={() => {
+                    this.localIds.map(localId => {
+                        weChat.uploadImage(localId).then(serverId => {
+                            this.serverIds.push(serverId);
+
                         })
-                    }}>上传图片
-                    </Button>
-                    {
-                        this.serverIds.length ?
-                            <Button plain className="btn" onClick={() => {
-                                this.serverIds.map(serverId => {
-                                    weChat.downloadImage(serverId).then(downloadId => {
-                                        this.downloadIds.push(downloadId);
-                                        this.tip5 = true
-                                    })
+                    })
+                }}>上传图片
+                </Button>
+                {
+                    this.serverIds.length ?
+                        <Button plain className="btn" onClick={() => {
+                            this.serverIds.map(serverId => {
+                                weChat.downloadImage(serverId).then(downloadId => {
+                                    this.downloadIds.push(downloadId);
+                                    this.tip5 = true
                                 })
+                            })
 
-                            }}>上传成功,请点击下载图片</Button> : null
+                        }}>上传成功,请点击下载图片</Button> : null
+                }
+                <Toptips show={this.tip1}>{this.networkType}</Toptips>
+                <Toptips type="info" show={this.tip2}>{`经度 ：${this.longitude} 纬度 ：${this.latitude}`}</Toptips>
+                <Dialog type="ios" show={this.tip3} className="dialog" buttons={[
+                    {
+                        type: 'primary',
+                        label: 'Ok',
+                        onClick: () => {
+                            this.tip3 = false
+                        }
                     }
-                    <Toptips show={this.tip1}>{this.networkType}</Toptips>
-                    <Toptips type="info" show={this.tip2}>{`经度 ：${this.longitude} 纬度 ：${this.latitude}`}</Toptips>
-                    <Dialog type="ios" show={this.tip3} className="dialog" buttons={[
-                        {
-                            type: 'primary',
-                            label: 'Ok',
-                            onClick: () => {
-                                this.tip3 = false
-                            }
+                ]}>
+                    <span className="title">{address}</span>
+                    <div id="allmap" className="map">{null}</div>
+                </Dialog>
+                <Dialog type="ios" show={this.tip4} className="dialog" buttons={[
+                    {
+                        type: 'primary',
+                        label: '没毛病',
+                        onClick: () => {
+                            this.tip4 = false
                         }
-                    ]}>
-                        <span className="title">{address}</span>
-                        <div id="allmap" className="map">{null}</div>
-                    </Dialog>
-                    <Dialog type="ios" show={this.tip4} className="dialog" buttons={[
-                        {
-                            type: 'primary',
-                            label: '没毛病',
-                            onClick: () => {
-                                this.tip4 = false
-                            }
+                    }
+                ]}>{this.localIds.map(src => <img src={src}/>)}</Dialog>
+                <Dialog type="ios" show={this.tip5} className="dialog" buttons={[
+                    {
+                        type: 'primary',
+                        label: '一点毛病没有',
+                        onClick: () => {
+                            this.tip5 = false
                         }
-                    ]}>{this.localIds.map(src => <img src={src}/>)}</Dialog>
-                    <Dialog type="ios" show={this.tip5} className="dialog" buttons={[
-                        {
-                            type: 'primary',
-                            label: '一点毛病没有',
-                            onClick: () => {
-                                this.tip5 = false
-                            }
-                        }
-                    ]}>{this.downloadIds.map(src => <img src={src}/>)}</Dialog>
-                </div> : null
+                    }
+                ]}>{this.downloadIds.map(src => <img src={src}/>)}</Dialog>
+            </div>
         )
     }
 }
